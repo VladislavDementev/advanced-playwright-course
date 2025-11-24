728da9f7bafd48596c19cd737b56ddd07c475921
unknown <User@N-DKS-IZH-2279.lanit.lanit.ru>
2025-11-24 20:05:20 +0300

    new tests for mech

---
+++ tests/mech-tests/mech-test-2.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://mech.moscow/');
+  await page.getByRole('button', { name: 'Поиск' }).click();
+  await page.getByPlaceholder('Поиск').click();
+  await page.getByPlaceholder('Поиск').fill('Пуховик');
+// Замена на локатор по классу - выбираем первый результат
+  await page.locator('[class*="lg:hover:bg-grey"]').first().click();
+  await page.getByRole('button', { name: 'Добавить в корзину' }).click();
+  await page.locator('.grid.grid-cols-3.items-center.gap-2 > button:nth-child(3)').click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).nth(3).click();
+  await page.locator('#head').getByRole('link').filter({ hasText: /^$/ }).click();
+});
+
---
+++ tests/mech-tests/mech-test-3.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://mech.moscow/');
+  await page.getByRole('button', { name: 'Новинки' }).click();
+  await page.getByRole('button', { name: 'Фильтры' }).click();
+  await page.getByRole('checkbox', { name: 'coach1' }).check();
+  await page.getByRole('button', { name: 'Применить' }).click();
+  //замена на локатор по класс
+  await page.locator('//*[@id="__nuxt"]/div/main/div/div[2]/div[3]/div/article/div[3]/a/h3').click();
+  await page.getByRole('button', { name: 'Проверить наличие' }).click();
+  await page.goBack();
+  //шаг на страницу назад
+  // await page.getByRole('button', { name: 'Фильтры' }).waitFor();
+  await page.getByRole('button', { name: 'Фильтры' }).click();
+  await page.getByRole('button', { name: 'Очистить фильтры' }).click();
+});
