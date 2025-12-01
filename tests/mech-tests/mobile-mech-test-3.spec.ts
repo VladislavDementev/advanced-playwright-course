736f3e41304d5a6a0688277ca56a9b56fe4dccdb
unknown <l-e-o-n-93@mail.ru>
2025-12-01 21:36:17 +0300

    add: test for mobile fersion tests/mobile-mech-test-3.spec.ts

---
+++ tests/mech-tests/mobile-mech-test-3.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use(devices['Pixel 7 landscape']);
+
+test('mech-test-3', async ({ page }) => {
+  await page.goto('https://mech.moscow/');
+  await page.getByRole('button', { name: 'Новинки' }).click();
+  await page.getByRole('button', { name: 'Фильтры' }).click();
+  await page.getByRole('checkbox', { name: 'coach1' }).check();
+  await page.getByRole('button', { name: 'Применить' }).click();
+  //замена на локатор по класс
+  await page.locator('//*[@id="__nuxt"]/div/main/div/div[2]/div[3]/div/article/div[3]/a/h3').click();
+  await page.getByRole('button', { name: 'Проверить наличие' }).click();
+  await page.goBack();
+  //шаг на страницу назад
+  //комментарий для тестирования гита
+  await page.getByRole('button', { name: 'Фильтры' }).click();
+  await page.getByRole('button', { name: 'Очистить фильтры' }).click();
+  await page.goBack();
+});
